52aab3f8c6fb2e12323fc535e5568050facb6bca
AnkitRaj23 <ankit012345raj@gmail.com>
2024-06-23 19:35:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let abc= document.querySelector("h5");
+let xyz= document.querySelector("#add");
+temp=0;
+xyz.addEventListener('click', function(){
+if(temp==0){
+    abc.innerHTML="Following";
+    abc.style.fontWeight=600;
+    xyz.innerHTML="Unfollow";
+    temp=1;
+} else{
+    abc.innerHTML="Suggested For You";
+    abc.style.fontWeight=600;
+    xyz.innerHTML="Follow";
+    temp=0;
+}
+})
+
